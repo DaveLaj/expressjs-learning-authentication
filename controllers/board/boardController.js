d790e08a93c2544bab33d5ad434114f6f4101576
Dave <lapojapod@gmail.com>
2024-04-25 16:37:37 +0800

    made a gitignore

--- controllers/board/boardController.js
+++ controllers/board/boardController.js
@@ -6,8 +6,6 @@ db.query = util.promisify(db.query);
 
 exports.showBoard = function (req, res){
     user = req.session.user;
-    console.log(user , "showboard");
-    console.log(process.env.NODE_ENV);
     res.render('board', { user: user });
 }
 
